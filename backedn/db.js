4c274b8b1ecb823eea80ac814211ccabf105e019
Shaik Abzal <106366006+shaikabzal9676@users.noreply.github.com>
2024-08-22 15:08:19 +0530

    Update db.js

--- backedn/db.js
+++ backedn/db.js
@@ -1,12 +1,1 @@
-// const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://localhost:27017/myapp', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => {
-//   console.log('Connected to MongoDB');
-// }).catch((err) => {
-//   console.error('Error connecting to MongoDB', err);
-// });
-
-// module.exports = mongoose;
