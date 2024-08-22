5e911fe71d74ec33fa28d6120f0803a1a67d75d0
Shaik Abzal <106366006+shaikabzal9676@users.noreply.github.com>
2024-08-22 16:04:19 +0530

    Update app.js

--- backedn/app.js
+++ backedn/app.js
@@ -123,5 +123,5 @@ app.delete('/users/:_id', async (req, res) => {
 });
 
   
-const PORT = 5000;
+const PORT =process.env.PORT|| 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
