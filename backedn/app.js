89761717d7e4ced494bc2950354dbfd501f0c80b
Shaik Abzal <106366006+shaikabzal9676@users.noreply.github.com>
2024-08-22 11:45:24 +0530

    Update app.js

--- backedn/app.js
+++ backedn/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Connect Mongoose
 
-mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://shaikabzal626:pe41hbP2TJQma2WM@cluster1.gtbu2.mongodb.net/nyapp?retryWrites=true&w=majority&appName=Cluster1', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
